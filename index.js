6e178d5aad40ba2bd5ed632e4b42a044a1ac725a
muthukumarathithan <muthu.kumaravm8@gmail.com>
2021-03-20 18:35:59 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,3 +10,49 @@ var x, y;
 x = 5;
 y = 6;
 document.getElementById("demo").innerHTML = x + y;
+
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("demo").innerHTML = "The value of z is: " + z;
+
+const price4 = 10;
+const price5 = 20;
+const price6 = price4 + price5;
+document.getElementById("demo").innerHTML = price6;
+
+var price1 = 100;
+var price1 = 200;
+var price3 = price1 + price1;
+console.log(price3);
+document.getElementById("demo").innerHTML = price3;
+
+let price7 = 10;
+let price8 = 20;
+let price9 = price7 + price8;
+console.log(price9);
+
+var price10 = 10;
+let price11 = 100;
+console.log(price11);
+
+var carName;
+console.log(carName);
+
+var carName = "hello";
+console.log(carName);
+
+var carName = "Total";
+carName = "data";
+console.log(carName);
+
+const data = "control";
+console.log(data);
+
+let point = "point";
+point = "pop";
+console.log(point);
+
+age = 20;
+job = "it";
+console.log(job);
