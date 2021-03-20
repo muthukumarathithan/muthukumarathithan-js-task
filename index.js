1612383e46c5805184e7b7de4b9e70c8767342ba
muthukumarathithan <muthu.kumaravm8@gmail.com>
2021-03-20 15:29:49 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function myFunction() {
   document.getElementById("demo7").innerHTML = "Paragraph changed.";
 }
+
+var x;
+x = 6;
+document.getElementById("demo").innerHTML = x;
+
+var x, y;
+x = 5;
+y = 6;
+document.getElementById("demo").innerHTML = x + y;
